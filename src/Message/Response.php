6abcf96af161425ce8d901c0f21fef401f16b59f
Nguyen Nhat Tam <ntam444@gmail.com>
2020-06-26 14:38:42 +0700

    add method get message by code

--- src/Message/Response.php
+++ src/Message/Response.php
@@ -33,6 +33,63 @@ class Response extends AbstractResponse
     /**
      * {@inheritdoc}
      */
+    public function getMessageByCode(): ?string
+    {
+        switch ($this->getCode()) {
+            case "0" :
+                $result = "Approved";
+                break;
+            case "1" :
+                $result = "Bank Declined";
+                break;
+            case "3" :
+                $result = "Merchant not exist";
+                break;
+            case "4" :
+                $result = "Invalid access code";
+                break;
+            case "5" :
+                $result = "Invalid amount";
+                break;
+            case "6" :
+                $result = "Invalid currency code";
+                break;
+            case "7" :
+                $result = "Unspecified Failure ";
+                break;
+            case "8" :
+                $result = "Invalid card Number";
+                break;
+            case "9" :
+                $result = "Invalid card name";
+                break;
+            case "10" :
+                $result = "Expired Card";
+                break;
+            case "11" :
+                $result = "Card Not Registed Service(internet banking)";
+                break;
+            case "12" :
+                $result = "Invalid card date";
+                break;
+            case "13" :
+                $result = "Exist Amount";
+                break;
+            case "21" :
+                $result = "Insufficient fund";
+                break;
+            case "99" :
+                $result = "User cancel";
+                break;
+            default :
+                $result = "Failured";
+        }
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function getMessage(): ?string
     {
         return $this->data['vpc_Message'] ?? null;
